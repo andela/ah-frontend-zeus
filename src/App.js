f3502f311fdefdc4ffef8290003174ccc35d694c
Isaac Ongebo <isaacongebo@gbo.local>
2019-01-08 17:27:06 +0300

    chore(setup): create an empty react app
    - Add .gitignore, README.md, and LICENSE.txt
    - Initialize repository as an npm project (adds package.json)
    - Add dependencies for Webpack, Babel and React to package.json
    - Create .babelrc to configure Babel
    - Add Webpack configuration to webpack.config.js
    - Create basic application files (index.html, index.js, App.js)
    - Configure hot reloading in App.js
    
    [Finishes #162163122]

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Authors Haven!</h1>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
