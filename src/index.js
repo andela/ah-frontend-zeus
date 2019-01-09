ee52f30e43945bbfe0975cd2027f3c5ef707287c
Eric Ebulu <ekumamait@Erics-MacBook-Pro.local>
2019-01-09 15:10:58 +0300

    chore(react-redux): set up redux
    - This chore installs redux and creates the appropriate folder/file structure for React/Redux workflow
    
    [Finishes #162163123]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
