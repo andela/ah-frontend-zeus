639aba910c4d98e5456e1a94e6764d52ca9b05ba
Kayabula Alex Joseph <kayabulaalexjoseph@ALEX.local>
2019-01-10 21:11:37 +0300

    test3

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 const Home = () => {
   return <h4> Share your ideas with us. Get the audience you desire. </h4>;
-}
+};
 
 export default Home;
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -3,6 +3,6 @@ import Login from '../components/Login';
 
 const LoginView = () => {
   return <Login />;
-}
+};
 
 export default LoginView;
