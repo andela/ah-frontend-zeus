fcbec8890d7e27ddfcaddd90e4f49240418c1eb4
Richard Nabaasa <nabaasarichard@gmail.com>
2019-01-10 19:37:06 +0300

    check hound

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 
+const hound = 'this const is not used';
+
 class App extends Component {
   render() {
+    console.log('hound fails');
     return (
       <div>
-        <h1>Welcome to Authors Haven - Zeus Team</h1>
+        <h1>Welcome to Authors Haven - Zeus Team 2</h1>
       </div>
     );
   }
