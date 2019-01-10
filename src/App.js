701f721c5baa8296eafb269aca3fcc9fefb9ee50
Ayesiga Lindsey Patra <lindsey@Ayesigas-MacBook-Pro.local>
2019-01-10 20:20:35 +0300

    chore(react router dom): setup react router version 4 in the project.
    - create the components and views folder in the src folder.
    - add routes in the app.js file.
    
    [Finishes #162163126]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomeView from './views/HomeView';
+import LoginView from './views/LoginView';
 import './App.scss';
+import NavView from './views/NavView';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to Authors Haven - Zeus Team</h1>
+        <BrowserRouter>
+          <div>
+            <NavView />
+            <Switch>
+              <Route path="/" component={HomeView} exact />
+              <Route path="/login" component={LoginView} exact />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
