e1e66d5e643c1a22e66d7935cc68da5c32c86d99
lindseyme <30689391+lindseyme@users.noreply.github.com>
2019-01-14 05:36:34 -0800

    bug(react-router): bug fix for react router (#21)
    - Add htmlwebpackplugin in the webpack config file
    
    [Finishes #163190086]

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
@@ -25,14 +25,15 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    historyApiFallback: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin(),
+  new HtmlWebpackPlugin({
+    template:'public/index.html'
+  })]
 };
