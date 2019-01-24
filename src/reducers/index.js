3e35e0c311ec133b2cd27ce07d2b43d9a69931f3
Nabaasa Richard <nabaasarichard@gmail.com>
2019-01-24 15:53:04 +0300

    clean up develop git status (#35)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
-import testReducer from './TestReducer';
+import TestReducer from './TestReducer';
 import ErrorReducer from './ErrorReducer';
 const rootReducer = combineReducers({
-  auth: authReducer,
-  users: testReducer,
-  errors: errorReducer
+  auth: AuthReducer,
+  users: TestReducer,
+  errors: ErrorReducer
 });
 
 export default rootReducer;
