6851eb83b104f26186618951acec15961247d343
lindseyme <30689391+lindseyme@users.noreply.github.com>
2019-01-23 23:59:05 -0800

    feat(login): users should be able to Login to Authors Haven (#29)
    - create a login component
    - create action and reducer for login
    
    [Finishes #162163131]

---
+++ src/__tests__/actions/authActions.test.js
@@ -0,0 +1,22 @@
+import { loginUser, registerUser } from '../../actions/authActions';
+import fetchMock from 'fetch-mock';
+import { createMemoryHistory } from 'history';
+
+const data = {
+  user: {
+    email: 'patra@mail.com',
+    username: 'patra',
+    token: 'eyJ0eXAiOiJ1QiLCJh.eyJpZCI6MTQzMTAxN30.DUkUyNISyfAt6jM_sLV7gWo'
+  }
+};
+
+describe('request', () => {
+  it('Should return all tasks', async () => {
+    fetchMock.post('https://zeus-staging.herokuapp.com/api/users/login/', data);
+    const userData = {};
+    const history = createMemoryHistory('/articles');
+    const resp = await loginUser(userData, history);
+    const expected = new Promise(() => {});
+    expect(resp()).toEqual(expected);
+  });
+});
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,12 @@
-const initialState = {
-    isAuthenticated: false,
-    user: {}
-};
-export default function(state = initialState, action){
-    switch (action.type) {
-        
-        default:
-        return state;
+import { SET_CURRENT_USER } from '../constants/ActionTypes';
 
-    }
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
